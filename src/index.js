222b49eeac77db21d31e91887ee5e97653a44c43
BSKOM <b.shelestua@gmail.com>
2021-02-28 22:52:53 +0200

    fix: fix toReadable function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 module.exports = function toReadable (number) {
-  const s0019 = [
-    '', 'one', 'two', 'three', 'four',
-    'five', 'six', 'seven', 'eight', 'nine',
-    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
-    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const s0019 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 
+  'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
   const s2090 = ['', '', 'twenty', 'thirty', 'forty',
-    'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+      'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   let s = ''
-  if (number > 99){
-    s += s2090[number / 100] + ' hundred '
-  }
-  if ((number % 100) > 19) {
-    s += s2090[number % 100] + ' ' + s0019[number % 100]
-  } else if (number === 0) 
-  s='zero'
+  if (number > 99)s += s0019[Math.floor(number / 100)] + ' hundred '
+  if ((number % 100) > 19)
+  s += s2090[Math.floor(number % 100 / 10)] + ' ' + s0019[(number % 100) % 10]
+  else if (number === 0) s = 'zero'
   else s += s0019[number % 100]
-  return s
+  return s.trim()
 }
