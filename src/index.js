aca319b1ae067610a4849bc02a50b474c0520916
BSKOM <b.shelestua@gmail.com>
2021-02-28 21:04:27 +0200

    init: start toReadable V 1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+  const s0019 = [
+    '', 'one', 'two', 'three', 'four',
+    'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const s2090 = ['', '', 'twenty', 'thirty', 'forty',
+    'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  let s = ''
+  if (number > 99){
+    s += s2090[number / 100] + ' hundred '
+  }
+  if ((number % 100) > 19) {
+    s += s2090[number % 100] + ' ' + s0019[number % 100]
+  } else if (number === 0) 
+  s='zero'
+  else s += s0019[number % 100]
+  return s
 }
